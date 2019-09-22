31206bda93a7395edaffb0fc5924435b2166abbc
Gerald Aburto Urroz <geraldaburto@pop-os.localdomain>
2019-09-21 21:00:06 -0600

    Updates

---
+++ precache-manifest.f7ef23a5e00569c85e7e9b681c5a0dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805e709584dabb9bc8a8a53fed8662c8",
+    "url": "/kodeinlunch/index.html"
+  },
+  {
+    "revision": "e5c8e0c3a10123b9e171",
+    "url": "/kodeinlunch/static/css/2.6e33773d.chunk.css"
+  },
+  {
+    "revision": "52f8be92e6440a906e7d",
+    "url": "/kodeinlunch/static/css/main.05a0ee05.chunk.css"
+  },
+  {
+    "revision": "e5c8e0c3a10123b9e171",
+    "url": "/kodeinlunch/static/js/2.d78be11e.chunk.js"
+  },
+  {
+    "revision": "52f8be92e6440a906e7d",
+    "url": "/kodeinlunch/static/js/main.d7e1d133.chunk.js"
+  },
+  {
+    "revision": "c26ea91deddb852f61e0",
+    "url": "/kodeinlunch/static/js/runtime-main.ec4d03f7.js"
+  }
+]);
